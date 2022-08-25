e98b01a91ecd91d23cbb81aa684ae177b9600932
Maicon <maiconthales34@hotmail.com>
2022-08-25 16:55:53 -0300

    Add Config

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-//Install express server
-const express = require('express');
-const path = require('path');
-
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
