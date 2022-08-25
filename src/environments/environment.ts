6a9611561efb7fc7bc445688e5d57de26f73ead2
Maicon <maiconthales34@hotmail.com>
2022-08-25 16:31:12 -0300

    Alter config.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,28 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
 
+  AUTH_API: "https://freefood-api.herokuapp.com/",
+  USER_CONTROLLER: "user",
+  RESTAURANT_CONTROLLER: "restaurant",
+  MENU_CONTROLLER: "menu",
+  ADDRESS_CONTROLLER: "address",
+  REQUEST_CONTROLLER: "request",
+  
+  REDIRECT_DASHBOARD: "/dashboard",
+  REDIRECT_DASHBOARD_TWO: "/dashboardAllMenu",
+  REDIRECT_AUTHENTICATION: "/authentication",
+  REDIRECT_RESTAURANT: "/restaurant",
+  REDIRECT_MENU: "/menu",
+  REDIRECT_RESQUEST: "/request",
+  REDIRECT_USER_EDIT: "/profile-edit",
+  REDIRECT_SHOPPING_CAR: "/shopping-car",
+
+  IMAGE_DEFAULT: "../assets/img/avatar/no_have_image.jpg",
+
+  LANGUAGE_OPTIONS: ['en', 'pt-br']
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
